31e1a9159b223264e95c1c0d3e254a9c25e49761
Bina Khalid <binaanwar@gmail.com>
2017-09-25 23:09:36 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,35 @@
+$(function () {
+
+    var buzzer = $("#buzzer")[0],
+        count = parseInt($('#num').html());
+    count2 = parseInt($('#breakNum').html());
+    buzzer.play();
+
+    $('#m5time').click(function () {
+        if (count > 0) {
+            count -= 5;
+            $("#num").html(count);
+        }
+        event.preventDefault();
+    });
+    $('#a5time').click(function () {
+
+        count += 5;
+        $("#num").html(count);
+        event.preventDefault();
+    });
+ $('#m5Break').click(function () {
+        if (count2 > 0) {
+            count2 -= 5;
+            $("#breakNum").html(count2);
+        }
+        event.preventDefault();
+    });
+    $('#a5Break').click(function () {
+
+        count2 += 5;
+        $("#breakNum").html(count2);
+        event.preventDefault();
+    });
+
+});
